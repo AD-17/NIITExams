982aac6fe70ae4f18cd824219eb6ec789e9acf1a
AD-17 <106794653+AD-17@users.noreply.github.com>
2022-06-10 16:07:14 +0530

    Add files via upload
    2. Define a class called  FourWheeler consisting of  name, cc, safety, price. 
    If cc is less than 1000 then it will or greater than 4000, then it will generate VehicleException with information like ‘It is a Two Wheeler, not Four Wheeler’ or ‘It is a Vehicle not a Train ’ respectively. 
    Similarly if the safety if not 5 then it’ll generate UnsafetyException with a message ‘Sorry, Invalid Safty features’. 
    The VehicleException and UnsafetyExceptions are user defined exception classes. 
    Create at least 4 object’s of FourWheer class in main() method of Factory class to validate the FourWheeres cc and safety.

---
+++ DemoFourWheller.java
@@ -0,0 +1,25 @@
+package com.vehicle;
+
+public class DemoFourWheller {
+
+		public static void main(String[] args) {
+			FourWheller c1 = new FourWheller("Honda City", 1200, 5, 1500000);
+			c1.display();
+			c1.VehicleException();
+			c1.UnsafetyException();
+			FourWheller c2 = new FourWheller("Hyundai Verna", 1500, 5, 1400000);
+			c2.display();
+			c2.VehicleException();
+			c2.UnsafetyException();
+			FourWheller c3 = new FourWheller("Honda Jazz", 900, 4, 500000);
+			c3.display();
+			c3.VehicleException();
+			c3.UnsafetyException();
+			FourWheller c4 = new FourWheller("Maruti Ciaz", 1300, 6, 1000000);
+			c4.display();
+			c4.VehicleException();
+			c4.UnsafetyException();
+
+		}
+	}
+
